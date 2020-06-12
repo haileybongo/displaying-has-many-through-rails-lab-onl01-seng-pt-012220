dc5399f1a2b10bada5366139e7008edda8ac45f1
haileybongo <haileyebongo@gmail.com>
2020-06-11 22:10:07 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'doctor/show'
+
+  get 'doctor/settings'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
   resources :patients
